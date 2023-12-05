14ebdcac4e7b4f93bd686177d2e9409257875d99
Le Hoang Nam <lehoangnam.31123@gmail.com>
2023-12-05 12:08:19 +0700

    Create UI for project base test send email with java

---
+++ src/main/java/com/mycompany/testsendemail/TestSendEmail.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testsendemail;
+
+/**
+ *
+ * @author lehoa
+ */
+public class TestSendEmail {    
+    public static void main(String[] args) {
+        FormSendEmail formSendEmail = new FormSendEmail();
+        formSendEmail.setVisible(true);
+        formSendEmail.setLocationRelativeTo(null);
+    }
+}
